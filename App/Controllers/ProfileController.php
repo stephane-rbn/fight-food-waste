37e712862db9598eaef146d91b87b37d10aecdf0
Stéphane <rabena.stephane@gmail.com>
2019-04-14 01:13:49 +0200

    feat(profile controller): get the current logged-in user + display their profile data

---
+++ App/Controllers/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Auth;
+use Core\View;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Profile controller
+ */
+class ProfileController extends AuthenticatedController
+{
+    /**
+     * Show the profile
+     *
+     * @return void
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function show()
+    {
+        View::renderTemplate('Profile/show.html.twig', [
+            'user' => Auth::getUser(),
+        ]);
+    }
+}
