abb32df4b98de72edd372c52fc7ed6a4f4c5f6ca
Stéphane <rabena.stephane@gmail.com>
2019-04-13 22:47:38 +0200

    feat(schema database): add account activation fields to the donor table in the database

--- schema_build.sql
+++ schema_build.sql
@@ -2,23 +2,23 @@ DROP TABLE IF EXISTS `remembered_logins`;
 DROP TABLE IF EXISTS `donors`;
 
 CREATE TABLE `donors` (
-  `id`                    INTEGER PRIMARY KEY AUTO_INCREMENT,
-  `uniqueId`             VARCHAR(32) NOT NULL,
-  `firstName`            VARCHAR(60) NOT NULL,
-  `middleName`           VARCHAR(60),
-  `lastName`             VARCHAR(60) NOT NULL,
-  `email`                 VARCHAR(255) NOT NULL UNIQUE,
-  `companyName`          VARCHAR(100),
-  `phoneNumber`          VARCHAR(60) NOT NULL,
-  `password`              VARCHAR(255) NOT NULL,
+  `id`                  INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `uniqueId`            VARCHAR(32) NOT NULL,
+  `firstName`           VARCHAR(60) NOT NULL,
+  `middleName`          VARCHAR(60),
+  `lastName`            VARCHAR(60) NOT NULL,
+  `email`               VARCHAR(255) NOT NULL UNIQUE,
+  `companyName`         VARCHAR(100),
+  `phoneNumber`         VARCHAR(60) NOT NULL,
+  `password`            VARCHAR(255) NOT NULL,
   `passwordResetHash`   VARCHAR(64) DEFAULT NULL UNIQUE,
   `passwordResetExpiry` DATETIME DEFAULT NULL,
-#   `token`        VARCHAR(64),
-#   `confirmation` VARCHAR(64),
+  `activationHash`      VARCHAR(64) DEFAULT NULL UNIQUE,
+  `isActive`            BOOLEAN NOT NULL DEFAULT 0,
+  `createdAt`           DATETIME,
+  `updatedAt`           DATETIME
 #   `pickupPlaceId` INTEGER REFERENCES `pickup_places` (`id`),
 #   `donorTypeId` INTEGER REFERENCES `donor_types` (`id`),
-  `createdAt`   DATETIME,
-  `updatedAt`   DATETIME
 );
 
 CREATE TABLE `remembered_logins` (
